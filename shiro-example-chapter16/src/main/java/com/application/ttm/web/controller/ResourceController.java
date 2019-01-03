b3c336be39a9367fb6375b7f6645f4edd48aaaf5
tangtaiming <accountnum@sina.cn>
2019-01-03 23:29:56 +0800

    feat(loation):资源功能提交
    author:tangtaiming

---
+++ shiro-example-chapter16/src/main/java/com/application/ttm/web/controller/ResourceController.java
@@ -0,0 +1,54 @@
+package com.application.ttm.web.controller;
+
+import com.application.ttm.entity.Resource;
+import com.application.ttm.service.ResourceService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+/**
+ * <p>User: tangtaiming
+ * <p>Date: 2018-12-30
+ * <p>Version: 1.0
+ */
+@Controller
+@RequestMapping("/resource")
+public class ResourceController {
+
+    @Autowired
+    private ResourceService resourceService;
+
+    @RequiresPermissions("resource:view")
+    @RequestMapping(method = RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("resourceList", resourceService.findAll());
+        return "resource/list";
+    }
+
+    @RequiresPermissions("resource:create")
+    @RequestMapping(value = "/{parentId}/appendChild", method = RequestMethod.GET)
+    public String showAppendChild(@PathVariable("parentId") Long parentId, Model model) {
+        Resource parent = resourceService.findOne(parentId);
+        model.addAttribute("parent", parent);
+        Resource child = new Resource();
+        child.setParentId(parentId);
+        child.setParentIds(parent.makeSelfAsParentIds());
+        model.addAttribute("resource", child);
+        model.addAttribute("op", "新增子节点");
+        return "resource/edit";
+    }
+
+    @RequiresPermissions("resource:create")
+    @RequestMapping(value = "/{parentId}/appendChild", method = RequestMethod.POST)
+    public String create(Resource resource, RedirectAttributes redirectAttributes) {
+        resourceService.createResource(resource);
+        redirectAttributes.addFlashAttribute("msg", "新增子节点成功");
+        return "redirect:/resource";
+    }
+
+}
