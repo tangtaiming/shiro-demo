bd41b0df3a051f5b1beda7d4a80e72134f4e856c
tangtaiming <accountnum@sina.cn>
2019-04-07 14:35:21 +0800

    feat(loation):shiro demo17 测试用例
    author:tangtaiming

--- shiro-example-chapter17-server/src/main/java/com/application/ttm/shiro/Constants.java
+++ shiro-example-chapter17-server/src/main/java/com/application/ttm/shiro/Constants.java
@@ -9,5 +9,6 @@ public class Constants {
 
     public static String RESOURCE_SERVER_NAME = "chapter17-server";
     public static final String INVALID_CLIENT_DESCRIPTION = "客户端验证失败，如错误的client_id/client_secret。";
+    public static final String ERROR_CODE = "错误的授权码";
 
 }
