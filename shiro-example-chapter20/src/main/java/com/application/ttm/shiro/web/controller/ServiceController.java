dacbe9e5641d07cf018551a26a121fc87a72fe37
tangtaiming <accountnum@sina.cn>
2019-04-24 07:15:44 +0800

    feat(loation):提交 无状态集成
    author:tangtaiming

--- shiro-example-chapter20/src/main/java/com/application/ttm/shiro/web/controller/ServiceController.java
+++ shiro-example-chapter20/src/main/java/com/application/ttm/shiro/web/controller/ServiceController.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("")
 public class ServiceController {
 
     @RequestMapping("/hello")
     public String hello1(String[] param1, String param2) {
+        System.out.println("------");
         return "hello" + param1[0] + param1[1] + param2;
     }
 }
