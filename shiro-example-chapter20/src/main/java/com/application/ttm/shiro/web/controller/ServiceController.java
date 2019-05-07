7a8b782f174385062a5ea86becb1eba9c024d4b0
tangtaiming <accountnum@sina.cn>
2019-05-07 14:10:12 +0800

    feat(loation):解决---->
    author:tangtaiming

--- shiro-example-chapter20/src/main/java/com/application/ttm/shiro/web/controller/ServiceController.java
+++ shiro-example-chapter20/src/main/java/com/application/ttm/shiro/web/controller/ServiceController.java
@@ -1,13 +1,16 @@
 package com.application.ttm.shiro.web.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/hello")
 public class ServiceController {
 
-    @RequestMapping("/hello")
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
     public String hello1(String[] param1, String param2) {
         System.out.println("------");
         return "hello" + param1[0] + param1[1] + param2;
